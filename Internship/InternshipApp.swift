ab5722799569562bb6eec43ad1d0692958ca5f20
Sneha <snehtyagi9810@gmail.com>
2023-12-12 19:20:47 +0530

    Initial Commit

---
+++ Internship/InternshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternshipApp.swift
+//  Internship
+//
+//  Created by Sneh on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InternshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
